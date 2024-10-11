a31cace4ea8d7588c8341d0fe34b26910c087782
Alisa <zxyan8732@gmail.com>
2024-10-11 14:12:49 +0700

    Add files via upload

--- cogs/Misc/restart.py
+++ cogs/Misc/restart.py
@@ -1,1 +1,34 @@
-n
+import discord
+from discord.ext import commands
+import os
+import sys
+
+class Restart(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Hybrid Command untuk restart
+    @commands.hybrid_command(name="restart", with_app_command=True, description="Restart the bot (Hanya Admin dan Helper!)")
+    @commands.is_owner()
+    @commands.has_permissions(administrator=True)
+    async def restart(self, ctx: commands.Context):
+        embed = discord.Embed(
+            title="Bot Restart",
+            description="The bot is restarting...",
+            color=discord.Color.darker_grey()
+        )
+        embed.set_footer(text="Please wait while the bot restarts.")
+        
+        # Send the embed message
+        if isinstance(ctx, discord.Interaction):
+            await ctx.send(embed=embed, ephemeral=True)
+        else:
+            await ctx.send(embed=embed)
+        
+        await self.bot.close()
+
+        # Restart bot
+        os.execv(sys.executable, ['python'] + sys.argv)
+
+async def setup(bot):
+    await bot.add_cog(Restart(bot))  # Menambahkan cog ke bot
